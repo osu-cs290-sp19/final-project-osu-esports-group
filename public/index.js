7608fc1aa78ace1e634735bedcf484300c09a2eb
Anthony Martin <martian8@oregonstate.edu>
2019-06-08 16:54:18 -0700

    Add client side javascript

--- public/index.js
+++ public/index.js
@@ -1,3 +1,75 @@
+function addMember(){
+  var name = document.getElementById('join-name-input').value.trim();
+  var email = document.getElementById('join-email-input').value.trim();
+  var username = document.getElementById('join-username-input').value.trim();
+  var year = document.getElementById('join-year-input').value.trim();
+  var checkboxes = document.getElementsByTagName('input');
+  if(name && email && username && year && (checkboxes[0].checked || checkboxes[1].checked || checkboxes[2].checked || checkboxes[3].checked)){
+    var count = 0;
+    var trueCheckbox;
+    var gameid;
+    for (var i = 0; i < checkboxes.length; i++){
+      if(checkboxes[i].checked){
+        trueCheckbox = checkboxes[i].value
+        count++;
+      }
+    }
+    if(count>1){
+      alert("Check only one box!");
+    } else {
+    var postRequest = new XMLHttpRequest();
+    var requestURL = '/join/addMember';
+    postRequest.open('POST', requestURL);
+    if(trueCheckbox==="League of Legends"){
+      gameid = "league";
+    } else if(trueCheckbox==="Overwatch"){
+      gameid = "overwatch";
+    } else if(trueCheckbox==="Rocket League"){
+      gameid = "rocketleague";
+    } else {
+      gameid = "csgo";
+    }
+    var requestBody = JSON.stringify({
+      name: name,
+      username: username,
+      year: year,
+      email: email,
+      game: trueCheckbox,
+      gameid: gameid
+    });
+
+    postRequest.addEventListener('load', function (event) {
+      if (event.target.status === 200) {
+        console.log("Success");
+        for (var i = 0; i < checkboxes.length; i++){
+          checkboxes[i].checked = false;
+        }
+        var clearName = document.getElementById('join-name-input');
+        var clearEmail = document.getElementById('join-email-input');
+        var clearYear = document.getElementById('join-year-input');
+        var clearUsername = document.getElementById('join-username-input');
+        clearName.value = '';
+        clearEmail.value = '';
+        clearYear.value = '';
+        clearUsername.value = '';
+      } else {
+        alert("Error storing photo: " + event.target.response);
+      }
+    });
+
+    postRequest.setRequestHeader('Content-Type', 'application/json');
+    postRequest.send(requestBody);
+  }
+  } else {
+      alert("One of the fields is missing");
+  }
+}
+
 window.addEventListener('DOMContentLoaded', function () {
   console.log("DOM conted loaded, Adding event listeners");
+  var submitBtn = document.querySelector('.join-accept-button');
+  if(submitBtn){
+    submitBtn.addEventListener('click', addMember);
+  }
+
 });
--- public/peopleCardTemplate.js
+++ public/peopleCardTemplate.js
@@ -3,7 +3,6 @@
 templates['peopleCard'] = template({"compiler":[7,">= 4.0.0"],"main":function(container,depth0,helpers,partials,data) {
     var helper, alias1=depth0 != null ? depth0 : (container.nullContext || {}), alias2=helpers.helperMissing, alias3="function", alias4=container.escapeExpression;
 
-<<<<<<< HEAD
   return "<div class=\"peopleCard\">\r\n  <p>Name: "
     + alias4(((helper = (helper = helpers.name || (depth0 != null ? depth0.name : depth0)) != null ? helper : alias2),(typeof helper === alias3 ? helper.call(alias1,{"name":"name","hash":{},"data":data}) : helper)))
     + "</p>\r\n  <p>Year: "
@@ -24,12 +23,5 @@ templates['homeCard'] = template({"compiler":[7,">= 4.0.0"],"main":function(cont
     + "\"><img class=\"game-icon\" src=\""
     + alias4(((helper = (helper = helpers.url || (depth0 != null ? depth0.url : depth0)) != null ? helper : alias2),(typeof helper === alias3 ? helper.call(alias1,{"name":"url","hash":{},"data":data}) : helper)))
     + "\" /></a>\r\n  </div>\r\n</section>\r\n";
-=======
-  return "<div class=\"peopleCard\">\r\n  <p>Game: "
-    + alias4(((helper = (helper = helpers.game || (depth0 != null ? depth0.game : depth0)) != null ? helper : alias2),(typeof helper === alias3 ? helper.call(alias1,{"name":"game","hash":{},"data":data}) : helper)))
-    + "</p>\r\n  <p>Username: "
-    + alias4(((helper = (helper = helpers.username || (depth0 != null ? depth0.username : depth0)) != null ? helper : alias2),(typeof helper === alias3 ? helper.call(alias1,{"name":"username","hash":{},"data":data}) : helper)))
-    + "</p>\r\n</div> \r\n";
->>>>>>> 3884a1632f685f292d695c1231bcdee4b7b830be
 },"useData":true});
 })();
